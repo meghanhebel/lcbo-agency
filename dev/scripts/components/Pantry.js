93558485f498690287a772130f65c4cdfe6fe17e
Amie Everett <amie.everett@gmail.com>
2017-11-28 17:37:03 -0500

    added components, firebase, axios, etc

---
+++ dev/scripts/components/Pantry.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Pantry extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Pantry</h1>
+            </div>
+        );
+    }
+}
+
+export default Pantry;
