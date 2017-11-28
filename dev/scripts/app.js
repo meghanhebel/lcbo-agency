4bd4e8628c3740d5224b5b34c21acaf27baf5dcd
Meghan Hebel <meghanrhebel@gmail.com>
2017-11-28 14:16:04 -0500

    Added ReactRouterDom

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router, Route, Link, NavLink, Switch } from 'react-router-dom';
 
 class App extends React.Component {
     render() {
