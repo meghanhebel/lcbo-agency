41543ffbbe6aeb1d084cab0511b61d3ae6d87f01
Amie Everett <amie.everett@gmail.com>
2017-11-29 10:51:06 -0500

    adding some Results functionality, call to API and display result

--- dev/scripts/components/Results.js
+++ dev/scripts/components/Results.js
@@ -1,10 +1,57 @@
 import React from 'react';
+import axios from 'axios';
 
 class Results extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            wines: []
+        }
+    }
+
+    componentDidMount() {
+        const access_key = 'MDo5ODRjMDU2Ni1kNTBhLTExZTctYjFmYS1lN2UwOGZlNzE3OWY6WFJBVXV1Q2FkWDdBUkQ5aUtxc0ZYejl3ZTVCaDU0emFYRG56';
+        axios.get(`http://lcboapi.com/products?`, {
+            params: {
+                dataType: 'json',
+                q: 'wine',
+                // where_not: 'is_dead,is_discontinued',
+                access_key
+            }
+        }).then((res) => {
+            console.log(res.data.result);
+            this.setState({
+                wines: res.data.result
+            });
+        });
+    }
+    
     render() {
         return (
-            <div>
+            <div className='searchResults'>
                 <h1>Results</h1>
+                <ul>
+                {this.state.wines.map((wine) => {
+                    console.log('categ ', wine.primary_category);
+                    return (
+                        <div key={wine.id} className='searchResults-wineResult'>
+                        {(wine.primary_category === 'wine') ? 
+                        <li>
+                            <img src={wine.image_thumb_url} alt={`image of ${wine.name}, a ${wine.secondary_category}`}/>
+                            <figcaption>
+                                <h3>{wine.name}</h3>
+                                <h4>{wine.varietal}</h4>
+                                <h4>${Math.round(wine.price_in_cents * .01 * 100 )/ 100}</h4>
+                                <h6>{wine.secondary_category}</h6>
+                                <h6>{wine.sugar_content}</h6>
+                                <h6>{wine.style}</h6>
+                                {wine.description ? <h6>{wine.description}</h6> : ''}
+                            </figcaption>
+                        </li> 
+                        : '' }
+                    </div>
+                )})}
+                </ul>
             </div>
         );
     }
