32b145327c59956da91dec5b433dbf5610d9c067
Amie Everett <amie.everett@gmail.com>
2017-11-29 12:29:36 -0500

    made some changes to how each wine result is displayed, added button to add to pantry

--- dev/scripts/components/Results.js
+++ dev/scripts/components/Results.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import { BrowserRouter as Router, Route, Link, NavLink, Switch } from 'react-router-dom';
 
 class Results extends React.Component {
     constructor() {
@@ -10,11 +11,13 @@ class Results extends React.Component {
     }
 
     componentDidMount() {
+        let searchParams = '';
+        // this variable to be filled with whatever the user enters
         const access_key = 'MDo5ODRjMDU2Ni1kNTBhLTExZTctYjFmYS1lN2UwOGZlNzE3OWY6WFJBVXV1Q2FkWDdBUkQ5aUtxc0ZYejl3ZTVCaDU0emFYRG56';
         axios.get(`http://lcboapi.com/products?`, {
             params: {
                 dataType: 'json',
-                q: 'wine',
+                q: `wine+${searchParams}`,
                 where_not: 'is_dead,is_discontinued',
                 access_key
             }
@@ -32,22 +35,33 @@ class Results extends React.Component {
                 <h1>Results</h1>
                 <ul>
                 {this.state.wines.map((wine) => {
-                    console.log('categ ', wine.primary_category);
+                    const secondCateg = wine.secondary_category;
+                    let typeWine = '';
+                        if (secondCateg.match(/Red/)) {
+                            typeWine = 'red';
+                        } else if (secondCateg.match(/White/)) {
+                            typeWine = 'white';
+                        } else {
+                            typeWine = 'rose';
+                        }
                     return (
-                        <div key={wine.id} className='searchResults-wineResult'>
+                        <div key={wine.id} className={`wineResult ${typeWine}`}>
                         {(wine.primary_category === 'Wine') ? 
                         // need this to filter out anything that is NOT primary category Wine
                         <li>
                             <img src={wine.image_thumb_url} alt={`image of ${wine.name}, a ${wine.secondary_category}`}/>
                             <figcaption>
                                 <h3>{wine.name}</h3>
-                                <h4>{wine.varietal}</h4>
-                                <h4>${Math.round(wine.price_in_cents * .01 * 100 )/ 100}</h4>
-                                <h6>{wine.secondary_category}</h6>
-                                <h6>{wine.sugar_content}</h6>
-                                <h6>{wine.style}</h6>
-                                {wine.description ? <h6>{wine.description}</h6> : ''}
-                                {/* many of them don't have a description */}
+                                <h6>
+                                    <span>{wine.varietal}</span>
+                                    <span>{wine.sugar_content}</span>  
+                                    <span>${Math.round(wine.price_in_cents * .01 * 100) / 100}</span>
+                                </h6>
+                                {(wine.description || wine.style) ? (wine.description ? <h6>{wine.description}</h6> : <h6>{wine.style}</h6>) : ''} 
+                                {/* nested ternary-- if wine has a description OR a style 
+                                    --> check if it has desc, display that, else style
+                                    --> if it doesnt have either, display nothing */}
+                            <NavLink to="/pantry"><button>Add to pantry</button></NavLink>
                             </figcaption>
                         </li> 
                         : '' } 
