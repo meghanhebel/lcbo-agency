494a6188f8659febf4323d3968143b5109cc8fa7
Craig Anderson <craigstuartanderson@gmail.com>
2017-12-05 17:00:31 -0500

    footer styles

--- dev/scripts/components/Navigation.js
+++ dev/scripts/components/Navigation.js
@@ -41,7 +41,6 @@ export default class Navigation extends React.Component {
                                 <img src="./public/images/rack.svg" alt="" />
                             </NavLink>
                             <h4>Pantry</h4>
-                            <div className="circle"></div>
                         </li>
                         <li>
                             <NavLink to="/marketplace"activeClassName = "current">
@@ -49,7 +48,6 @@ export default class Navigation extends React.Component {
                                 <img src="./public/images/market.svg" alt=""/>
                             </NavLink>
                             <h4>Search</h4>
-                            <div className="circle"></div>
                         </li>
                         <li>
                             <a href="#" onClick={this.logOutMsg}>
@@ -57,7 +55,6 @@ export default class Navigation extends React.Component {
                                 <img src="./public/images/truck.svg" alt="" />
                             </a>
                             <h4>Logout</h4>
-                            <div className="circle"></div>
                         </li>
                     </ul>
                     <div className="logOutMsg modal" id="logOutMsg">
