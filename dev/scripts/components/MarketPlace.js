0d5ab5e62f3dcde50acf7cbc2527d075022d4a54
Amie Everett <amie.everett@gmail.com>
2017-12-05 21:03:24 -0500

    fixed axios maybe

--- dev/scripts/components/MarketPlace.js
+++ dev/scripts/components/MarketPlace.js
@@ -35,15 +35,16 @@ export default class MarketPlace extends React.Component {
     makeDataCall(keywords) {
         console.log(`Called with the keyword ${keywords}`)
         const access_key = 'MDo5ODRjMDU2Ni1kNTBhLTExZTctYjFmYS1lN2UwOGZlNzE3OWY6WFJBVXV1Q2FkWDdBUkQ5aUtxc0ZYejl3ZTVCaDU0emFYRG56';
-        axios.get(`http://lcboapi.com/products?`, {
-            params: {
-                dataType: 'json',
-                q: keywords,
-                where_not: 'is_dead,is_discontinued',
-                per_page: 100,
-                access_key
-            }
+        axios.get(`https://lcboapi.com/products?`, {
+                params: {
+                        dataType: 'json',
+                        q: keywords,
+                        where_not: 'is_dead,is_discontinued',
+                        per_page: 100,
+                        access_key
+                    }
         }).then((res) => {
+            console.log('result ',res);
             console.log('result from API ',res.data.result);
             this.setState({
                 wineResults: res.data.result.filter(wine => (wine.primary_category === "Wine" && wine.sugar_content)),
